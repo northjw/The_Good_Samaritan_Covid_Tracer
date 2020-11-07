84693d8ed2c90d3ff050094777d1c324eee4f197
ron erlih <ronerlih@gmail.com>
2020-11-07 11:32:33 -0500

    changed process.env var

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ let sequelize;
 if (config.use_env_variable) {
 	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-	sequelize = new Sequelize(config.database, config.username, config.LOCAL_MYSQL_PASS, config);
+	sequelize = new Sequelize(config.database, config.username, process.env.LOCAL_MYSQL_PASS, config);
 }
 
 fs.readdirSync(__dirname)
