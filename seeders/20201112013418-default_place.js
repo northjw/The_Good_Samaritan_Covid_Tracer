a35b744f03c2e65f7b75e5fc8762b33a3f2370a9
yerson <yerson.a06@gmail.com>
2020-11-11 21:30:05 -0500

    places database draft

--- seeders/20201112013418-default_place.js
+++ seeders/20201112013418-default_place.js
@@ -3,16 +3,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('places', [{
-    
+      place: 1,
+      name: "Mudd",
+      category: "Building",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }]);
   },
-
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('places', null, {});
   }
 };
