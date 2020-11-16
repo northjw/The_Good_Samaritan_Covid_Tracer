19ba0ee1113cba22424652240655741038226e26
Kentaro Kinoshita <mixmix0321@gmail.com>
2020-11-15 21:32:44 -0500

    added code for location

--- public/assets/js/location.js
+++ public/assets/js/location.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
+    var placedata = [];
     var searchbar = $('#search')
     searchbar.on('keyup', function(e){
-        console.log(e.target.value)
-    })
+        let searchedvalue = e.target.value;
+        const filteredPlaceData = placedata.filter((place_name) => {
+            return (
+                place_name.place_name.includes(searchedvalue)
+            );
+        });
+        filteredPlaceData.forEach(createGuessedValueTab)
+        // $.each(filteredPlaceData, createGuessedValueTab)
+    });
+   
+    function createGuessedValueTab(results) {
+        // console.log(data)
+            let guessedName = results.place_name;
+            let newDiv = $('<div>').addClass('dropdown-item').text(guessedName)
+            console.log(newDiv)
+            searchbar.addClass('dropdown-divider')
+            searchbar.append(newDiv);
+    }
+
     $.get("/api/place_data", function (data) {
-        console.log(data);
+        placedata = data;
     })
 });
