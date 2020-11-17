6af4df46543ebdfae3229ed94f3dcae2844bbc43
Kentaro Kinoshita <mixmix0321@gmail.com>
2020-11-16 20:39:24 -0500

    Set up email

--- routes/api-routes/emailRoutes.js
+++ routes/api-routes/emailRoutes.js
@@ -5,27 +5,38 @@ const router = express.Router();
 // const transporter = require('../config/transporter')
 
 router.post("/", function(req, res) {
-    console.log(req.body.email)
-    let testAccount = nodemailer.createTestAccount();
+    // let testAccount = nodemailer.createTestAccount((err, account) => {
+    //     if (err) {
+    //         console.error('Failed to create a testing account');
+    //         console.error(err);
+    //         return process.exit(1);
+    //     }
+    
+    // });
+        
     let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
+        service: "zoho",
         port: 587,
         secure: false,
         auth: {
-            type: 'OAuth2',
-            user: 'user@example.com',
-            clientId: '000000000000-xxx0.apps.googleusercontent.com',
-            clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
-            refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-            accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         },
-        tls: {
-            rejectUnauthorized:false
-        }
-      });
+        logger: true,
+        debug: false // include SMTP traffic in the logs
+    }
+//     {
+//         // default message fields
+
+//         // sender info
+//         // from: 'Nodemailer <example@nodemailer.com>',
+    
+  );
+
+    
       
       let mailOptions = {
-        from: "mixmix0321@gmail.com",
+        from: "goodsamaritinmailer@zohomail.com",
         to: req.body.email,
         subject: "ALERT!! PEOPLE WHO WENT TO THE SAME PLACE AS YOU HAVE JUST TESTED POSITIVE!!",
         text: "Thank You for using The Good Samaritin - Smart Covid 19 Tracer. This is an auto-alert that you might be at risk of injection of covid 19!"
