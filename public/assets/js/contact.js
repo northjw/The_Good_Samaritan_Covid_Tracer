a0a12e36cb8f829224736e27dfaffea8131dbb66
mini pony <northjw1@gmail.com>
2020-11-17 17:19:13 -0500

    contact us from compplete, about us page 75% done missing Kentaro's website, fixed drop down nav bar

---
+++ public/assets/js/contact.js
@@ -0,0 +1,24 @@
+// $(document).ready(function () {
+//     $( "#contactform" ).submit(function( event ) {
+//         alert( "Handler for .submit() called." );
+//         event.preventDefault();
+//       });
+// console.log("I am here")
+// // add post request
+// // function contactus( name, email, subject, message) {
+// //     $.post("/api/login", {
+// //       name: name,
+// //         email: email,
+// //       subject: subject,
+// //       message: message,
+// //     })
+// //       .then(function() {
+// //         window.location.replace("/location");
+// //         // If there's an error, log the error
+// //       })
+// //       .catch(function(err) {
+// //         console.log(err);
+// //       });
+// //   }
+// #fname.val
+// })
